eba521bd39bad1e5478b87782fe326292951afda
Bobby Hughes <bobbyhughes6316@gmail.com>
2023-07-06 10:21:30 -0400

    gifter

---
+++ Gifter/Repositories/IPostRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Gifter.Models;
+
+namespace Gifter.Repositories
+{
+    public interface IPostRepository
+    {
+        List<Post> GetAll();
+        Post GetById(int id);
+        void Add(Post post);
+        void Update(Post post);
+        void Delete(int id);
+        List<Post> GetAllWithComments();
+    }
+}
